7e9d8f3823dff6cb483673ecb034cd41ad65dbd6
armi <superigorek74@gmail.com>
2024-03-31 01:21:14 +0500

    Update model Task

--- api/main/models.py
+++ api/main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Task(models.Model):
+	class DaysOfWeek(models.TextChoices):
+		MONDAY = 'MON', "MONDAY"
+		TUESDAY = 'TUES', 'TUESDAY'
+		WEDNESDAY = 'WED', 'WEDNESDAY'
+		THURSDAY = 'THURS', 'THURSDAY'
+		FRIDAY = 'FRI', 'FRIDAY'
+		SATURDAY = 'SAT', 'SATURDAY'
+		SUNDAY = 'SUN', 'SUNDAY' 
 	parent_task = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
+	day = models.CharField(max_length=5, choices=DaysOfWeek)
 	text = models.CharField(max_length = 1000, blank=False, null=False)
-	expire = models.DateTimeField(null=True, blank=True)
 	done = models.BooleanField(default=False, blank=True)
 
 	def __str__(self):
