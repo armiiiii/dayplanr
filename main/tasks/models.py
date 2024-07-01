d424d723917dfe3fdc70f41e8177c8caef83c706
armi <superigorek74@gmail.com>
2024-07-02 00:05:00 +0500

    Rest framework was configured. DJANGO_CORS was configured

--- main/tasks/models.py
+++ main/tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from rest_framework.serializers import ModelSerializer
 
-week = {
+WEEK = {
     "SUNDAY": "SUNDAY",
     "MONDAY": "MONDAY", 
     "TUESDAY": "TUESDAY",
@@ -14,4 +15,10 @@ class Task(models.Model):
     order = models.PositiveSmallIntegerField(null=False, blank=False)
     task = models.CharField(max_length=250, null=False, blank=False)
     done = models.BooleanField(default=False, blank=True)
-    day = models.CharField(choices=week, blank=False, null=False, max_length=9)
+    day = models.CharField(choices=WEEK, blank=False, null=False, max_length=9)
+
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = "__all__"
--- main/tasks/views.py
+++ main/tasks/views.py
@@ -1,19 +1,22 @@
-import json
-
 from django.http import JsonResponse
-from django.core.serializers.json import DjangoJSONEncoder
 
-from .models import Task, week
+from rest_framework.parsers import JSONParser
+
+from .models import Task, TaskSerializer, WEEK
 
 
 def index(request):
     if request.method == 'POST':
-        req = request.POST
-        print(req)
-        Task.objects.create(task = req['task'], day = req['day'], order = req['order'])
-        return JsonResponse('200')
-    Week = {}
-    for d, day in week.items():
-        queryset = Task.objects.filter(day=d).order_by("order")
-        Week[day] = json.dumps(list(queryset), cls=DjangoJSONEncoder)
-    return JsonResponse([{"week": Week}])
+        data = JSONParser().parse(request)
+        serializer = TaskSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'GET':
+        week = {}
+        for d, day in WEEK.items():
+            serializer = TaskSerializer(Task.objects.filter(day=d).order_by("order"), many=True)
+            week[day] = serializer.data
+        return JsonResponse(f'{week}', safe=False)
+    return JsonResponse(f"Unsupported method {request.method}", status=501)
